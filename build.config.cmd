2ac721c59f10a29ce8de83b696d1a6b45337286b
Grigore Stefan <g_stefan@yahoo.com>
2020-11-02 02:31:06 +0200

    update

--- build.config.cmd
+++ build.config.cmd
@@ -3,4 +3,6 @@ rem Public domain
 rem http://unlicense.org/
 rem Created by Grigore Stefan <g_stefan@yahoo.com>
 
+set PRODUCT_NAME=installer-nodejs
 set PRODUCT_VERSION=14.15.0
+set PRODUCT_BASE=nodejs
