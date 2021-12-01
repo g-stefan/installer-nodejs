54898a5ffbd9bb7c875eddfecc17ed58c91cc25c
Grigore Stefan <g_stefan@yahoo.com>
2021-12-01 02:55:04 +0200

    Update

--- build/build.config.cmd
+++ build/build.config.cmd
@@ -4,5 +4,5 @@ rem http://unlicense.org/
 rem Created by Grigore Stefan <g_stefan@yahoo.com>
 
 set PRODUCT_NAME=installer-nodejs
-set PRODUCT_VERSION=14.16.0
+set PRODUCT_VERSION=16.13.0
 set PRODUCT_BASE=nodejs
